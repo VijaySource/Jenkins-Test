cde6026692599fda35e9b9ee0a6af7c2fbf956c3
YD Vijay Vittal <vijay.yeldonadesai@valuelabs.net>
2014-07-18 12:18:47 +0530

    initial commit
    initial commit

---
+++ JenkinsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by  on 17/07/14.
+//  Copyright (c) 2014 ValueLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
